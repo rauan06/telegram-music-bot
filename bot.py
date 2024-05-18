5d7986f25378a867368a97942ef804a2b37437eb
Rauan <138851982+rauan06@users.noreply.github.com>
2024-05-18 13:52:22 +0500

    Small changes

---
+++ bot.py
@@ -0,0 +1,23 @@
+import os
+import telebot
+from telebot import types
+import time, asyncio
+from pyrogram import Client, filters
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(commands=['clear_chat'])
+def clear_all_chat():
+    pass
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.infinity_polling()
